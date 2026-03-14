a4ae593b4fa06d410e303366324bf96e30c7340a
hodpel <920998354@qq.com>
2026-03-14 16:19:01 +0800

    fix(ui): 修复文章标题和发布时间的样式和交互问题
    - 为`<article>`标签添加了`group`类，使其成为一个交互组，以便其子元素可以响应父元素的交互效果。
    - 更新了`<h2>`标签的类，添加了`group-hover:text-theme`和`transition`类，使标题在交互组悬停时改变颜色，并添加过渡效果。
    - 修改了`<time>`标签的文本颜色，将浅灰色从`text-gray-600`更改为`text-gray-500`，以更好地匹配新的设计风格。

--- components/PostIems.tsx
+++ components/PostIems.tsx
@@ -8,16 +8,16 @@ import FormattedDate from '@/components/FormattedDate'
 const PostItem = ({ post }: { post: PageProperties }) => {
     return (
         <Link scroll={false} href={`${config.path}/${post.slug}`}>
-            <article key={post.id} className="mb-6 md:mb-8 flex flex-row gap-4 items-start">
+            <article key={post.id} className="group mb-6 md:mb-8 flex flex-row gap-4 items-start">
                 <div className="relative h-9 w-9 shrink-0">
                     <PageIcon post={post} />
                 </div>
                 <div className="flex-1 min-w-0">
                     <header className="flex flex-col justify-between md:flex-row md:items-baseline">
-                        <h2 className="flex items-center gap-2 text-lg md:text-3xl font-bold mb-2 cursor-pointer text-black dark:text-gray-100  min-w-0 flex-1 ">
+                        <h2 className="flex items-center gap-2 text-lg md:text-3xl font-bold mb-2 cursor-pointer text-black dark:text-gray-100 group-hover:text-theme min-w-0 flex-1 transition ">
                             <span className="wrap-break-word">{post.title}</span>
                         </h2>
-                        <time className="shrink-0 text-gray-600 dark:text-gray-400">
+                        <time className="shrink-0 text-gray-500 dark:text-gray-400">
                             <FormattedDate date={post.date} />
                         </time>
                     </header>
