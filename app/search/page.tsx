a4f3bb41e27dc72bca10c216cee78dc3c2e2ee58
hodpel <920998354@qq.com>
2026-03-14 17:56:46 +0800

    feat(app): 添加搜索和标签页的revalidate配置及默认函数
    添加了搜索页面的revalidate配置，设置为100，即页面内容每100秒重新验证一次，并添加了默认的异步函数Search，该函数接收一个包含page参数的对象。
    
    添加了标签页面的revalidate配置，设置为100，即页面内容每100秒重新验证一次，并添加了默认的异步函数Tag。

---
+++ app/search/page.tsx
@@ -0,0 +1,7 @@
+//@@WIP
+// export const revalidate = 1
+export const revalidate = 100
+
+export default async function Search({ params }: { params: { page: number } }) {
+    return <></>
+}
---
+++ app/tag/[tag]/page.tsx
@@ -0,0 +1,6 @@
+// export const revalidate = 1
+export const revalidate = 100
+
+export default async function Tag() {
+    return <></>
+}
